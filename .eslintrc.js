e18d0c3bdf5a51667e97e7382a97e5d671f18cf6
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-04 00:13:18 +0100

    Add needed dependencies and configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,39 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ["xo", "prettier"],
+  extends: ['xo', 'prettier'],
   overrides: [
     {
-      extends: ["xo-typescript", "prettier"],
-      files: ["*.ts", "*.tsx"],
+      extends: ['xo-typescript', 'prettier'],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "@typescript-eslint/consistent-type-definitions": [
-          "error",
-          "interface",
+        '@typescript-eslint/consistent-type-definitions': [
+          'error',
+          'interface',
         ],
-        "@typescript-eslint/indent": "off",
-        "@typescript-eslint/no-empty-function": "off",
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  IgnorePatterns: ['**/public/**/*.js'],
   rules: {
-    "no-unused-vars": [
-      "error",
+    indent: ['Error', 2],
+    'object-curly-spacing': ['Error', 'Always'],
+    'no-unused-vars': [
+      'error',
       {
-        vars: "all",
-        args: "after-used",
+        vars: 'all',
+        args: 'after-used',
         ignoreRestSiblings: true,
         argsIgnorePattern: /^_/.source,
-        caughtErrors: "all",
+        caughtErrors: 'all',
         caughtErrorsIgnorePattern: /^_$/.source,
       },
     ],
