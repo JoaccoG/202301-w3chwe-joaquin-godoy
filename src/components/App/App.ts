fb385ca0e70c1387a8b73de14300a0faed09c0f0
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-05 03:58:01 +0100

    Add card-list component using data algorithm

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,6 +1,10 @@
 import Component from '../Component/Component.js';
 import Header from '../Header/Header.js';
 import Title from '../Title/Title.js';
+import CardList from '../Card-List/Card-List.js';
+import { getPokemonList, pokemonsList } from '../../data/data.js';
+
+getPokemonList(0);
 
 export default class App extends Component {
   #childrens: Component[];
@@ -14,11 +18,15 @@ export default class App extends Component {
         ['Home', 'Favorites']
       ),
       new Title(this.domElement, './assets/pokemon-logo.svg', 'logo'),
+      new CardList(this.domElement, pokemonsList),
     ];
   }
 
   render(): void {
     super.render();
     this.#childrens.forEach((children) => children.render());
+    setTimeout(() => {
+      this.#childrens[2].render();
+    }, 500);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import App from './components/App/App.js';
-import Card from './components/Card/Card.js';
-import { getPokemonList, pokemonsList } from './data/data.js';
 
 const generateApp = new App(document.body);
 generateApp.render();
-
-getPokemonList(0);
-setTimeout(() => {
-  let a = new Card(document.body, pokemonsList[0]);
-  a.render();
-}, 500);
